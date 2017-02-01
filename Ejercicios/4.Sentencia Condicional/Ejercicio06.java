f34fda38fc7c4ae8660b368fc7c47914712f841d
Lucía Flores Padilla <lucia.flores.padilla@hotmail.com>
2017-02-01 09:57:51 +0100

    Del 1 al 6 de Condicionales

--- Ejercicios/4.Sentencia Condicional/Ejercicio06.java
+++ Ejercicios/4.Sentencia Condicional/Ejercicio06.java
@@ -6,24 +6,17 @@
 import java.util.Scanner;
 public class Ejercicio06 {
 	public static void main (String[] args) {
-		Scanner s = new Scanner (System.in);
-		double a,b;
+		Scanner es = new Scanner (System.in);
 		
-		System.out.println("Este programa resuelve una ecuación de primer grado de tipo: ax + b = 0");
-		
-		System.out.print("Introduce el valor de a: ");
-		a = s.nextDouble();
-		
-		System.out.print("Y ahora introduce el valor de b: ");
-		b = s.nextDouble();
+		double g = 9.81;
 
-		if (a == 0) {
-			System.out.println("La ecuación no tiene solución.");
-		
-		}else {
-			System.out.println("x = " + (-b/a));
-		}
+    System.out.println("Tiempo que tarda un objeto en caer.");
+    System.out.print("Introduzca la altura (en metros) desde la que cae el objeto: ");
+    double h = es.nextDouble();
 
+    double s = Math.sqrt(2*h/g);
+
+    System.out.printf("El objeto tarda %.2f segundos en caer.\n", s);
 
 	}//Cierra el public static
 	
