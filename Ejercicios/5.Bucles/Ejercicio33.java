3efbfb1211a44bfd849b83d15c16dec849d1bd89
Lucía Flores Padilla <lucia.flores.padilla@hotmail.com>
2017-03-30 12:32:19 +0200

    Ejercicios 33, 34, 35 bucles

---
+++ Ejercicios/5.Bucles/Ejercicio33.java
@@ -0,0 +1,29 @@
+/* Nombre: Lucía Flores Padilla
+ * Fecha: 25 de marzo de 2017
+ * Ejercicio 29 bucles
+ * Programación
+ */
+public class Ejercicio33 {
+  public static void main (String[] args) {
+    
+    System.out.println("Con este programa pitaremos una U: ");
+    System.out.print("Introduzca la altura que quieras: ");
+    int altura = Integer.parseInt(System.console().readLine());
+
+    //Aquí se pintan el palo de la derecha y el de la izquierda
+    for (int i = 1; i < altura; i++) {
+      System.out.print("* ");
+      for (int j = 2; j < altura; j++) {
+        System.out.print("  ");
+      }
+      System.out.println("*");
+    }
+
+    // Aquí se pinta la base
+    System.out.print("  ");
+    for (int i = 2; i < altura; i++) {
+      System.out.print("* ");
+    }
+
+  }
+}
