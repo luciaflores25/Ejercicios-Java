321cb8b735d6a886798687c93f64b1b07ce44bde
Lucía Flores Padilla <lucia.flores.padilla@hotmail.com>
2017-04-26 09:10:49 +0200

    Ejercicio 19 Números aleatorios

---
+++ Ejercicios/6.Números Aleatorios/Ejercicio19.java
@@ -0,0 +1,44 @@
+/*@author Lucía Flores Padilla
+ * Fecha: 26 de abril de 2017
+ * Ejercicio 19 Números Aleatorios
+ * Programación
+ */
+  
+public class Ejercicio19 {
+
+  public static void main(String[] args){
+    
+      int num;
+      int maximo;
+      int minimo;
+      int media;
+    media = 0;
+    
+    maximo = -100;
+    minimo = 300;
+    
+    for (int i = 0; i < 50; i++) {
+      
+      num = (int)((Math.random()*301) - 100);
+      
+      System.out.print(num + " ");
+      
+      if (num % 2 == 0) {
+        
+        if (num > maximo) {
+          maximo = num;
+        }
+      } else {
+        
+          if (num < minimo) {
+            minimo = num;
+          }
+      }
+      media = media + num;
+        }
+    
+    System.out.println("\nMáximo: " + maximo);
+    System.out.println("Minimo: " + minimo);
+    System.out.println("Media: " + (media/50));
+  }
+}
