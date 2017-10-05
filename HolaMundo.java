decbea5a0c511d9e35d5c878bda58bc97c872a5f
Lucía Flores Padilla <lucia.flores.padilla@gmail.com>
2017-10-05 10:38:55 +0200

    Añadido el programa Hola Mundo

---
+++ HolaMundo.java
@@ -0,0 +1,15 @@
+/**
+ * Programación
+ * Tema 1 Salida de datos por  pantalla
+ * Ejercicio 1
+ * 29 de septiembre de 2017
+ * 
+ * @author Lucía Flores Padilla
+ */
+
+public class HolaMundo {
+  public static void main(String[] args) {
+    System.out.println("Hola Mundo");
+    System.out.println("Programa realizado por Lucía Flores");
+  }
+}
