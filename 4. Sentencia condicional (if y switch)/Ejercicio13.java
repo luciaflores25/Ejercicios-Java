a789aa0ac0a52efc527304c785f58238884576bd
Lucía Flores Padilla <lucia.flores.padilla@gmail.com>
2017-10-19 08:42:32 +0200

    Añadido ejercicio 13

---
+++ 4. Sentencia condicional (if y switch)/Ejercicio13.java
@@ -0,0 +1,34 @@
+/**
+ * Programación
+ * Tema 4 Senttencia condicional (if y switch)
+ * Ejercicio 13
+ * 18 de octubre de 2017
+ * 
+ * Escribe un programa que ordene tres números enteros introducidos por
+   teclado.
+ * 
+ * @author Lucía Flores Padilla
+ */
+
+public class Ejercicio13 {
+  public static void main(String[] args) {
+    
+    System.out.println("Este programa va a ordenar 3 números introducidos");
+    System.out.print("Introduce un número entero: ");
+    int num1 = Integer.parseInt(System.console().readLine());
+    
+    System.out.print("Introduce otro: ");
+    int num2 = Integer.parseInt(System.console().readLine());
+    
+    System.out.print("Y ahora otro: ");
+    int num3 = Integer.parseInt(System.console().readLine());
+    
+    if ((num1 < num2) && (num2 < num3)){
+      System.out.print(num1 + " < " + num2 + " < " + num3);
+    }else if ((num2 < num1) && (num1 < num3)){
+      System.out.print(num2 + " < " + num1 + " < " + num3);   
+    }else if ((num3 < num2) && (num2 < num1)) {
+       System.out.print(num3 + " < " + num2 + " < " + num1);  
+    }
+  }
+}
