41169c8c554ed1ccd6c20cde9ef5e2badb7587ba
Lucía Flores Padilla <lucia.flores.padilla@gmail.com>
2017-10-20 09:44:27 +0200

    Arreglando algunos ejercicios

--- 4. Sentencia condicional (if y switch)/Ejercicio13.java
+++ 4. Sentencia condicional (if y switch)/Ejercicio13.java
@@ -23,12 +23,23 @@ public class Ejercicio13 {
     System.out.print("Y ahora otro: ");
     int num3 = Integer.parseInt(System.console().readLine());
     
-    if ((num1 < num2) && (num2 < num3)){
-      System.out.print(num1 + " < " + num2 + " < " + num3);
-    }else if ((num2 < num1) && (num1 < num3)){
-      System.out.print(num2 + " < " + num1 + " < " + num3);   
-    }else if ((num3 < num2) && (num2 < num1)) {
-       System.out.print(num3 + " < " + num2 + " < " + num1);  
+    if ((num1 < num2) && (num2 < num3)) {
+      System.out.println(num1+" < "+num2+" < "+num3);
+    }
+    if ((num1 < num3) && (num3 < num2)) {
+      System.out.println(num1+" < "+num3+" < "+num2);
+    }
+    if ((num2 < num1) && (num1 < num3)) {
+      System.out.println(num2+" < "+num1+" < "+num3);
+    }
+    if ((num2 < num3) && (num3 < num1)) {
+      System.out.println(num2+" < "+num3+" < "+num1);
+    }
+    if ((num3 < num2) && (num2 < num1)) {
+      System.out.println(num3+" < "+num2+" < "+num1);
+    }
+    if ((num3 < num1) && (num1 < num2)) {
+      System.out.println(num3+" < "+num1+" < "+num2);
     }
   }
 }
