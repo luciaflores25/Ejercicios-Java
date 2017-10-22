40912c4b88c19424571e4a78c0c20927731795aa
Lucía Flores Padilla <lucia.flores.padilla@gmail.com>
2017-10-22 13:53:05 +0200

    Añadido ejercicio 16

--- 4. Sentencia condicional (if y switch)/Ejercicio16.java
+++ 4. Sentencia condicional (if y switch)/Ejercicio16.java
@@ -17,6 +17,97 @@
 public class Ejercicio16 {
   public static void main(String[] args) {
     
+    int puntos= 0;
     
+    System.out.println("TEST DE INFIDELIDAD");
+    System.out.println("---------------------------");
+    
+    System.out.println("\nResponde verdadero (v) o falso (f) para saber si tu pareja es infiel");
+    
+    System.out.println("\n1. Tu pareja parece estar más inquieta de lo normal sin ningún motivo aparente. ");
+    String r1 = System.console().readLine();
+    
+    if (r1 .equals("v")){
+          puntos +=3;
+        }else{
+        }
+        
+    
+    System.out.println("\n2. Ha aumentado sus gastos de vestuario ");
+    String r2 = System.console().readLine();
+    if (r2 .equals("v")){
+          puntos +=3;
+        }else{
+        }
+        
+    System.out.println("\n3. Ha perdido el interés que mostraba anteriormente por ti ");
+    String r3 = System.console().readLine();
+    if (r3 .equals("v")){
+          puntos +=3;
+        }else{
+        }
+    
+    System.out.print("\n4. Ahora se afeita y se asea con más frecuencia (si es hombre)");
+    System.out.print(" o ahora se arregla el pelo y se asea con más frecuencia (si es mujer) ");
+    String r4 = System.console().readLine();
+    if (r4 .equals("v")){
+          puntos +=3;
+        }else{
+        }
+    
+    System.out.println("\n5. No te deja que mires la agenda de su teléfono móvil ");
+    String r5 = System.console().readLine();
+    if (r5 .equals("v")){
+          puntos +=3;
+        }else{
+        }
+    
+    System.out.println("\n6. A veces tiene llamadas que dice no querer contestar cuando estás tú delante ");
+    String r6 = System.console().readLine();
+    if (r6 .equals("v")){
+          puntos +=3;
+        }else{
+        }
+    
+    System.out.println("\n7. Últimamente se preocupa más en cuidar la línea y/o estar bronceado/a ");
+    String r7 = System.console().readLine();
+    if (r7 .equals("v")){
+          puntos +=3;
+        }else{
+        }
+    
+    System.out.println("\n8. Muchos días viene tarde después de trabajar porque dice tener mucho más trabajo ");
+    String r8 = System.console().readLine();
+    if (r8 .equals("v")){
+          puntos +=3;
+        }else{
+        }
+    
+    System.out.println("\n9. Has notado que últimamente se perfuma más ");
+    String r9 = System.console().readLine();
+    if (r9 .equals("v")){
+          puntos +=3;
+        }else{
+        }
+    
+    System.out.println("\n10. Se confunde y te dice que ha estado en sitios donde no ha ido contigo ");
+    String r10 = System.console().readLine();
+    if (r10 .equals("v")){
+          puntos +=3;
+        }else{
+        }
+        
+      System.out.println("tu puntuación es " + puntos);
+      
+      if ((puntos >= 0) && (puntos <=10)){
+        System.out.println("¡Enhorabuena! tu pareja parece ser totalmente fiel!");
+      } else if ((puntos >= 11) && (puntos <=22)){
+         System.out.print("Quizás exista el peligro de otra persona en su vida o ");
+         System.out.print("en su mente, aunque seguramente será algo sin importancia. No bajes la guardia.");
+      }else {
+         System.out.print("Tu pareja tiene todos los ingredientes para estar viviendo ");
+         System.out.print("un romance con otra persona. Te aconsejamos que indagues un ");
+         System.out.print("poco más y averigües que es lo que está pasando por su cabeza.");
+      }
   }
 }
