c130cfef9087bae548c381d239bc6e096409389c
Lucía Flores Padilla <lucia.flores.padilla@gmail.com>
2017-10-26 21:11:45 +0200

    Añadido ejercicio 2 bucles

---
+++ 5. Bucles/Ejercicio02.java
@@ -0,0 +1,22 @@
+/**
+ * Programación
+ * Tema 5 Bucles
+ * 26 de octubre de 2017
+ * Ejercicio 2
+ * 
+ * Muestra los números múltiplos de 5 de 0 a 100 utilizando un bucle while.
+ *  
+ * @author Lucía Flores Padilla
+ */
+
+public class Ejercicio02 {
+  public static void main(String[] args) {
+    
+    int i = 0;
+    
+    while(i <= 100) {
+      System.out.println(i);
+      i+=5;
+    }
+  }
+}
