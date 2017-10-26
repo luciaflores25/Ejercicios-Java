60b05ee345e7eb6c8830609ee7094cbb44efbb9a
Lucía Flores Padilla <lucia.flores.padilla@gmail.com>
2017-10-26 21:33:14 +0200

    Añadido ejercicio 4 bucles

---
+++ 5. Bucles/Ejercicio04.java
@@ -0,0 +1,20 @@
+/**
+ * Programación
+ * Tema 5 Bucles
+ * 26 de octubre de 2017
+ * Ejercicio 4
+ * 
+ *  Muestra los números del 320 al 160, contando de 20 en 20 hacia
+ *  atrás utilizando un bucle for.
+ *  
+ * @author Lucía Flores Padilla
+ */
+
+public class Ejercicio04 {
+  public static void main(String[] args) {
+    
+    for(int i = 320; i >= 160; i-=20) {
+      System.out.println(i);
+    }
+  }
+}
