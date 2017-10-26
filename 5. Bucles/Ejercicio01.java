22931b76d2dbfbcc0643dc1a9d52823c587f5101
Lucía Flores Padilla <lucia.flores.padilla@gmail.com>
2017-10-26 21:09:40 +0200

    Añadido primer ejercicio bucles

---
+++ 5. Bucles/Ejercicio01.java
@@ -0,0 +1,19 @@
+/**
+ * Programación
+ * Tema 5 Bucles
+ * Ejercicio 1
+ * 26 de octubre de 2017
+ * 
+ * Muestra los números múltiplos de 5 de 0 a 100 utilizando un bucle for.
+ *  
+ * @author Lucía Flores Padilla
+ */
+
+public class Ejercicio01 {
+  public static void main(String[] args) {
+    
+    for(int i = 0; i <= 100; i += 5) {
+      System.out.println(i);
+    }
+  }
+}
