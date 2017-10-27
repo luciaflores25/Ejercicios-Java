fc7be6f9e51e86e5198c294d923f2d41758ca6ce
Lucía Flores Padilla <lucia.flores.padilla@gmail.com>
2017-10-27 10:01:05 +0200

    Añadiendo clase Scanner a este ejercicio

--- 3. Lectura de datos desde teclado/Ejercicio01.java
+++ 3. Lectura de datos desde teclado/Ejercicio01.java
@@ -10,18 +10,21 @@
  * @author Lucía Flores Padilla
  */
 
+import java.util.Scanner;
+
 public class Ejercicio01 {
   public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
     
     System.out.print("Introduce un número entero: ");
-    int num1 = Integer.parseInt( System.console().readLine() );
+    int num1 = Integer.parseInt(s.nextLine());
     
-    System.out.print("Introduce otro número entero: \n");
-    int num2 = Integer.parseInt( System.console().readLine() );
+    System.out.print("Introduce otro número entero: ");
+    int num2 = Integer.parseInt(s.nextLine());
     
     int multiplicacion = num1 * num2;
     
-    System.out.println("\nEl primer número es: " + num1);
+    System.out.println("\n\nEl primer número es: " + num1);
     System.out.println("El segundo número es: " + num2);
     System.out.println("-------------------------------------------");
     System.out.println("El resultado de la multiplicación es: " + multiplicacion);
