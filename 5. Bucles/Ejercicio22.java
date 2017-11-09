03d891972cf934a8d8ef80ba21de268a106bc8d6
Lucía Flores <lucia.flores.padilla@gmail.com>
2017-11-09 10:32:19 +0100

    Acabado ejercicio 22 bucles

--- 5. Bucles/Ejercicio22.java
+++ 5. Bucles/Ejercicio22.java
@@ -14,5 +14,21 @@ public class Ejercicio22 {
   public static void main(String[] args) {
     Scanner s = new Scanner(System.in);
     
+    for(int numero = 2; numero < 100; numero++){
+      boolean esPrimo = true;
+      int comprobacion = 0;
+      
+      for(int j = 2; j < numero; j ++) {
+        
+        comprobacion = numero % j;
+        if (comprobacion == 0){
+          esPrimo = false;
+        }
+      }
+      if (esPrimo){
+        System.out.print(numero + " ");
+      }else{
+      }
+    }
   }
 }
