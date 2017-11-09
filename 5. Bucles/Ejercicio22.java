8d644d4c1df4a0f8b8d8039624453b3ee3247884
Lucía Flores <lucia.flores.padilla@gmail.com>
2017-11-09 09:41:43 +0100

    Añadido ejercicio 22 (en proceso)

---
+++ 5. Bucles/Ejercicio22.java
@@ -0,0 +1,18 @@
+/**
+ * Programación
+ * Tema 5 Bucles
+ * 9 de noviembre de 2017
+ * Ejercicio 22
+ * 
+ * 22. Muestra por pantalla todos los números primos entre 2 y 100, ambos incluidos.
+ *  
+ * @author Lucía Flores Padilla
+ */
+ 
+import java.util.Scanner;
+public class Ejercicio22 {
+  public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+    
+  }
+}
