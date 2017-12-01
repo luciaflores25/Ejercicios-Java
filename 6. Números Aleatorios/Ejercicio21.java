83b573e910de585e91e691d88f1814dd6845443a
Lucía Flores <lucia.flores.padilla@gmail.com>
2017-12-01 13:28:56 +0100

    Añadido ejercicio 21 números aleatorios

--- 6. Números Aleatorios/Ejercicio21.java
+++ 6. Números Aleatorios/Ejercicio21.java
@@ -8,7 +8,6 @@
        legal lanzadas al aire. Las monedas disponibles son de 1 céntimo, 2 céntimos,
        5 céntimos, 10 céntimos, 20 céntimos, 50 céntimos, 1 euro y 2 euros. Las dos
        posiciones posibles son cara y cruz.
-
         Ejemplo:
         2 céntimos - cara
         20 céntimos - cruz
