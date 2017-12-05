32c0fa95771c2d7ae46763501160f3270d7046a5
Lucía Flores <lucia.flores.padilla@gmail.com>
2017-12-05 18:08:39 +0100

    Añadido ejercicio 10 arrays

--- 7. Arrays/Ejercicio11.java
+++ 7. Arrays/Ejercicio11.java
@@ -2,19 +2,24 @@
  * Programación
  * Tema 7 Arrays
  * 5 de diciembre de 2017
- * Ejercicio 10
+ * Ejercicio 11
  * 
- * 10.  Escribe un programa que genere 20 números enteros aleatorios entre 0 y 100
-        y que los almacene en un array. El programa debe ser capaz de pasar todos
-        los números pares a las primeras posiciones del array (del 0 en adelante) y
-        todos los números impares a las celdas restantes. Utiliza arrays auxiliares si
-        es necesario.
+ * 11  Realiza un programa que pida 10 números por teclado y que los almacene en
+       un array. A continuación se mostrará el contenido de ese array junto al índice
+       (0 – 9) utilizando para ello una tabla. Seguidamente el programa pasará los
+       primos a las primeras posiciones, desplazando el resto de números (los que
+       no son primos) de tal forma que no se pierda ninguno. Al final se debe mostrar
+       el array resultante.
+       
+       Por ejemplo:
+       Array Inicial: 20   5   7   4  32   9   2  14  11  6 
+       Array final:    5   7   2  11  20   4  32   9  14  6
  *  
  * @author Lucía Flores Padilla
  */
  
 import java.util.Scanner;
-public class Ejercicio10 {
+public class Ejercicio11 {
   public static void main(String[] args) {
     Scanner s = new Scanner(System.in);
     
@@ -40,6 +45,8 @@ public class Ejercicio10 {
         contadorImpares++;
       }
     }
+    System.out.println("ContPares: " + contadorPares);
+    System.out.println("ContImpares: " + contadorImpares);
     System.out.println();
     System.out.print("\n                 Array de pares primero\n");
     
