40790777fc77d415d375efcc8cb268d6f476c30c
Lucía Flores <lucia.flores.padilla@gmail.com>
2018-01-12 13:20:03 +0100

    actualizado archivo ejercicios 1-8

--- 8. Funciones/matematicas/ManejoMatematicas.java
+++ 8. Funciones/matematicas/ManejoMatematicas.java
@@ -8,7 +8,8 @@
  * Ejercicios 1-14
  * Jueves, 11 de enero de 2018
  */
- package matematicas;
+ 
+package matematicas;
  
  public class ManejoMatematicas{
    
@@ -31,6 +32,19 @@
       }
       
       return (n == volteado);
+    }
+    
+    ///////////////  EJERCICIO 1 (FORMA SIMPLIFICADA) /////////////////
+   
+   /**
+    * Comprueba si un número es Capicúa o no
+    * @param n es el número introducido por el usuario
+    * @return <code>true</code> si el número introducido es capicúa 
+    * @return <code>false</code> si el número introducido no es capicúa
+    */
+    public static boolean esCapicua2(int n) {
+      
+      return n == voltea(n);
     }
     
     ///////////////  EJERCICIO 2 /////////////////
@@ -70,8 +84,8 @@
     
   /**
    * Dada la base y un exponente devuelve una potencia
-   * @param b es la base introducida por teclado
-   * @param e es el exponente introducido por teclado
+   * @param b es la base introducida de la potencia
+   * @param e es el exponente introducido de la potencia
    * @return <code>potencia</code> es el resultado de la potencia entre la base y el exponente 
    */
    public static int potencia(int b, int e){
@@ -148,7 +162,47 @@
     ///////////////  EJERCICIO 8 /////////////////
     
     /**
+     * Da la posición de la primera ocurrencia de un dígito dentro de un número
+     * entero. Si no se encuentra, devuelve -1.
+     * @param n es el número entero introducido por el usuario
+     * @param x es el número entero que se quiere buscar en el número ya introducido
+     * @return la posición del dígito introducido en el caso que se encuentre en él
      */
+     public static int posicionDeDigito(int n, int x) {
+      int posicion = 0;
+      int numero = voltea(n);
+      while (numero > 0) {
+        if (numero % 10 == x) {
+          return posicion;
+        }
+        numero /= 10;
+        posicion++;
+      }
+      
+      return -1;
+     }
+     
+     ///////////////  EJERCICIO 9 /////////////////
+    
+    /**
+     * Le quita a un número n dígitos por detrás (por la derecha).
+     * @param n es el número entero (long) introducido por el usuario.
+     * @param x es el número entero que se introduce para saber la cantidad de 
+     * números que se le va a quitar al introducido desde la derecha.
+     * @return es el número introducido ya acortado
+     */
+     public static long quitarPorDetras(long n, long x) {
+       int contador = 0;
+       
+       do{
+         while (contador < x) {
+           x /= 10;
+           contador++;
+         }
+       }while((n > 0) && (contador != x));
+     }
+   
+   
    
  }
  
