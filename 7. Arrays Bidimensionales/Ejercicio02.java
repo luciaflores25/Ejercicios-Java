a01ee86306f284b357903ad913d6faa1b81ac2d8
Lucía Flores <lucia.flores.padilla@gmail.com>
2018-01-21 14:28:48 +0100

    Subido ejercicio 2 de bidimensionales

--- 7. Arrays Bidimensionales/Ejercicio02.java
+++ 7. Arrays Bidimensionales/Ejercicio02.java
@@ -1,10 +1,10 @@
 /**
  * Programación
  * Tema 7 Arrays Bidimensionales
- * 19 de Noviembre de 2018
+ * 21 de Noviembre de 2018
  * Ejercicio 2
  * 
- * 1. Escribe un programa que pida 20 números enteros. Estos números se deben
+ * 2. Escribe un programa que pida 20 números enteros. Estos números se deben
  * introducir en un array de 4 filas por 5 columnas. El programa mostrará las
  * sumas parciales de filas y columnas igual que si de una hoja de cálculo se
  * tratara. La suma total debe aparecer en la esquina inferior derecha.
